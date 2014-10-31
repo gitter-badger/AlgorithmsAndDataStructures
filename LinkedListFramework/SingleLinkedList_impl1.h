addffd67d934056ac4e93e180defcedaca09235a
unknown <stefan_florea@MUCWS109.de.rtt.local>
2014-10-31 15:23:42 +0100

    - added insert sorted for linkedlist::singly::impl1

--- LinkedListFramework/SingleLinkedList_impl1.h
+++ LinkedListFramework/SingleLinkedList_impl1.h
@@ -19,24 +19,20 @@ template<typename T> class LinkedList
 		{
 
 		}
-
 		LinkedList(const std::vector<T>& elements) 
 		{
 			head = nullptr;
 			init(elements);
 		}
-
 		~LinkedList()
 		{
 			clear();
 		}
-
 		void init(const std::vector<T>& elements)
 		{
 			for(auto& e : elements)
 				addBack(e);
 		}
-
 		std::vector<T> flatten()
 		{
 			std::vector<T> flat;
@@ -46,7 +42,6 @@ template<typename T> class LinkedList
 
 			return flat;
 		}
-
 		int length()
 		{
 			int size = 0;
@@ -55,7 +50,6 @@ template<typename T> class LinkedList
 
 			return size;
 		}
-
 		void view()
 		{
 			std::cout << "head -> ";
@@ -63,13 +57,11 @@ template<typename T> class LinkedList
 				std::cout << n->data << " -> ";
 			std::cout << "nullptr" << "\n";
 		}
-
 		void addFront(const T& element)
 		{
 			Node<T>* newNode = new Node<T>(element, head);
 			head = newNode;
 		}
-
 		void addBack(const T& element)
 		{
 			Node<T>* n = head;
@@ -87,7 +79,6 @@ template<typename T> class LinkedList
 				n->next = newNode;
 			}
 		}
-
 		void clear()
 		{
 			Node<T>* n = head;
@@ -103,7 +94,6 @@ template<typename T> class LinkedList
 
 			head = nullptr;
 		}
-
 		void remove(const T& element)
 		{
 			Node<T>* currNode = head;
@@ -121,6 +111,37 @@ template<typename T> class LinkedList
 				else
 					prevNode->next = currNode->next;
 		}
+
+		void insertSorted(const T& element)
+		{
+			if(!head)
+			{
+				addFront(element);
+				return;
+			}
+
+			Node<T>* newNode = new Node<T>(element);
+
+			Node<T>* node = head;
+			Node<T>* prev = node;
+			while(node != nullptr && newNode->data > node->data)
+			{
+				prev = node;
+				node = node->next;
+			}
+
+			if(node == head)
+			{
+				newNode->next = head;
+				head = newNode;
+			}
+			else
+			{
+				newNode->next = prev->next;
+				prev->next = newNode;
+			}
+		}
+		
 
 	private:
 		Node<T>* head;			
--- LinkedListFramework/main.cpp
+++ LinkedListFramework/main.cpp
@@ -5,22 +5,24 @@
 #include <vector>
 #include <string>
 
-//using namespace linkedlist::singly::impl1; // impl1: head + null tail
+using namespace linkedlist::singly::impl1; // impl1: head + null tail
 //using namespace linkedlist::single::impl2; // impl2: head + tail
 //using namespace linkedlist::single::impl3; // impl3: dummy head + tail
 
-using namespace linkedlist::doubly::impl1; // impl1: head + null tail
+//using namespace linkedlist::doubly::impl1; // impl1: head + null tail
 
 
 int main()
 {
 	LinkedList<int> list;
-	list.addFront(1);
-	list.addFront(2);
-	list.addFront(3);
+	list.addFront(50);
+	list.addFront(40);
+	list.addFront(30);
+	list.addFront(20);
+	list.addFront(10);
+	list.insertSorted(1);
 
 	list.view();
-	list.viewReverse();
 
 	std::cout << "\n\n";
 	return 0;
