a387b0bfd2bfa901e32d2346f658dab22fcfcf16
Stefan-Tudor.FLOREA <Stefan-Tudor.FLOREA@3ds.com>
2014-11-06 16:07:48 +0100

    - added doubly linked list implementation with header + trail

---
+++ LinkedListFramework/DoublyLinkedList_impl3.h
@@ -0,0 +1,70 @@
+#pragma once
+
+#include "Node.h"
+
+#include <iostream>
+
+
+namespace linkedlist { namespace doubly { namespace impl3 {
+
+template<typename T> class LinkedList
+{
+	public:
+		LinkedList()
+		{
+			header = new Node<T>();
+			trail  = new Node<T>();
+
+			header->next = trail;
+			trail->prev  = header;
+		}
+
+		void addFront(const T& element)
+		{
+			Node<T>* newNode = new Node<T>(element);
+
+			if(header->next == trail)
+			{
+				newNode->next = trail;
+				newNode->prev = header;
+				header->next  = newNode;
+				trail->prev   = newNode;
+			}
+			else
+			{
+				newNode->next      = header->next;
+				header->next->prev = newNode;
+				newNode->prev	   = header;
+				header->next       = newNode;
+			}
+		}
+
+		void addBack(const T& element)
+		{
+		
+		}
+
+		void view()
+		{
+			std::cout << "sentinelHead -> ";
+			for(Node<T>* n = header->next; n != trail; n = n->next)
+				std::cout << n->data << " -> ";
+			std::cout << "sentinelTrail" << "\n";
+		}
+
+		void viewReverse()
+		{
+			std::cout << "sentinelTrail -> ";
+			for(Node<T>* n = trail->prev; n != header; n = n->prev)
+				std::cout << n->data << " -> ";
+			std::cout << "sentinelHead" << "\n";
+		}
+
+	private:
+		Node<T>* header;
+		Node<T>* trail;
+};
+
+} // namespace linkedlist
+} // namespace singly
+} // namespace impl3
--- LinkedListFramework/main.cpp
+++ LinkedListFramework/main.cpp
@@ -1,26 +1,29 @@
 #include "SingleLinkedList_impl1.h" // impl1: head + null
 #include "SingleLinkedList_impl2.h" // impl2: head + tail
-#include "SingleLinkedList_impl3.h" // impl2: sentinel head + sentinel tail
+#include "SingleLinkedList_impl3.h" // impl3: sentinel head + sentinel tail
 
 #include "DoublyLinkedList_impl1.h" // impl1: head + null
-#include "DoublyLinkedList_impl2.h" // impl1: head + tail
+#include "DoublyLinkedList_impl2.h" // impl2: head + tail
+#include "DoublyLinkedList_impl3.h" // impl3: sentinel head + sentinel tail
 
 #include <iostream>
 #include <vector>
 #include <string>
 
 
-using namespace linkedlist::singly::impl3; 
+using namespace linkedlist::doubly::impl3; 
 
 
 int main()
 {
 	LinkedList<int> list;
-	list.addBack(1);
-	list.addBack(2);
-	list.addBack(3);
+	list.addFront(1);
+	list.addFront(2);
+	list.addFront(3);
 
 	list.view();
+	std::cout << "\n";
+	list.viewReverse();
 
 	std::cout << "\n\n";
 	return 0;
