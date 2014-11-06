8ea034c9cd473b49a57bd498fa781ab135f40649
Stefan-Tudor.Florea <StefanFlorea.Tudor@gmail.com>
2014-11-06 22:23:57 +0100

    - simplified postfix expression

--- Stack/main.cpp
+++ Stack/main.cpp
@@ -4,43 +4,52 @@
 #include <iostream>
 #include <string>
 
+//================================================================================================================================================================================================================================================================
+namespace convert
+{
+	int to_int(char ch)
+	{
+		if(isdigit(ch))
+			return ch - '0'; // simple conversion of char -> int
+		else
+			return -1;
+	}
 
+	int evaluate(int a, int b, char operation)
+	{
+		int result = 0;
+
+		if(operation == '+')
+			result = a + b;
+
+		if(operation == '-')
+			result = a - b;
+
+		if(operation == '*')
+			result = a * b;
+
+		if(operation == '/')
+			result = a / b;
+
+		return result;
+	}
+}
+
+//================================================================================================================================================================================================================================================================
 int evaluatePostfixExpression(const std::string& expression)
 {
 	ArrayStack stack(expression.size());
 
 	for(auto ch : expression)
-	{
 		if(isdigit(ch))
-		{
-			int nr = (int)(ch - '0');
-			stack.push(nr);
-		}
+			stack.push(convert::to_int(ch));
 		else
-		{
-			int result;
-
-			if(ch == '+')
-				result = stack.pop() + stack.pop();
-
-			if(ch == '-')
-				result = stack.pop() - stack.pop();
-
-			if(ch == '*')
-				result = stack.pop() * stack.pop();
-
-			if(ch == '/')
-				result = stack.pop() / stack.pop();
-
-			stack.push(result);
-		}
-	}
+			stack.push(convert::evaluate(stack.pop(), stack.pop(), ch));
 
 	return stack.top();
 }
 
-
-
+//================================================================================================================================================================================================================================================================
 int main()
 {
 	std::cout << std::endl << evaluatePostfixExpression("598+46**7+*");
