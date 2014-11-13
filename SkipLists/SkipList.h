7cf50f4bcdeddb569e855a82c2c58410227098a5
Stefan-Tudor.FLOREA <Stefan-Tudor.FLOREA@3ds.com>
2014-11-13 15:34:15 +0100

    - first attemtp at skip lists implementation. Total fail :|

--- SkipLists/SkipList.h
+++ SkipLists/SkipList.h
@@ -1,26 +1,43 @@
 #pragma once
 
+#include <vector>
+#include <random>
+
+
 class SkipList
 {
 	public:
 		SkipList();
 
 		void insert(int element);
+
 		bool find(int element);
 		void view();
 
+
 	private:
+		void insert(int element, int listLevel);
+
+		bool flipCoin();
+
+
 		class Node
 		{
 			public:
 				Node(int data = 0) : data(data)
 				{
+					const int maxSize = 13;
 
+					for(int i = 0; i < maxSize; i++)
+						next.push_back(nullptr);
 				}
 
-				Node* next;
+				std::vector<Node*> next;
 				int data;
 		};
 
+		std::mt19937 engine;
+		std::uniform_int_distribution<int> distribution;
 		Node* head;
+		int maxLevel;
 };
--- SkipLists/main.cpp
+++ SkipLists/main.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
+#include "SkipList.h"
+
 
 int main()
 {
+	SkipList skipList;
+	for(int i = 0; i < 60; i++)
+		skipList.insert(rand() % 100);
+
+	skipList.view();
+
 	return 0;
 }
