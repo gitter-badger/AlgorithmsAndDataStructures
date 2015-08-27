adc10330dcb1b670e3b707304c29ec379fa13ede
Stefan-Tudor.FLOREA <stefanflorea.tudor@gmail.com>
2015-08-27 22:10:57 +0200

    Add method 2 for evaluation of recurssive postfix expression

--- RecursiveExpressionEvaluation/main.cpp
+++ RecursiveExpressionEvaluation/main.cpp
@@ -33,10 +33,23 @@ int eval(const std::string& expression, int& position)
 }
 
 //----------------------------------------------------------------------------------------------------------------------------------------------
+int eval2(const std::string& expression, int& position)
+{
+    char op = expression[position++];
+
+    if(std::isdigit(op))
+    {
+        return op - '0';
+    }
+
+    return apply(op, eval(expression, position), eval(expression, position));
+}
+
+//----------------------------------------------------------------------------------------------------------------------------------------------
 int main()
 {
     int pos = 0;
-    std::cout << std::endl << eval("*+7**46+895", pos);
+    std::cout << std::endl << eval2("*+7**46+895", pos);
 
     std::cout << std::endl << "\n\n";
     return 0;
