796fc74fc359406c115dfcfc101690335953144a
Stefan-Tudor.FLOREA <stefanflorea.tudor@gmail.com>
2015-09-16 23:03:55 +0200

    Add optimized insertion search

--- BasicSorting/OptimizedInsertionSort.cpp
+++ BasicSorting/OptimizedInsertionSort.cpp
@@ -10,5 +10,26 @@ OptimizedInsertionSort::OptimizedInsertionSort(const std::vector<Item>& items) :
 //======================================================================================================================
 void OptimizedInsertionSort::sort(std::vector<Item>& items, int left, int right)
 {
+    // move smallest sentinel to first position
+    for (size_t i = right - 1; i > left; i--)
+    {
+        if (items[i].getKey() < items[i-1].getKey())
+        {
+            std::swap(items[i], items[i-1]);
+        }
+    }
 
+    for (size_t i = left + 1; i < right; i++)
+    {
+        int j = i;
+        Item current = items[i];
+
+        while ( current.getKey() < items[j-1].getKey())
+        {
+            items[j] = items[j-1];
+            j--;
+        }
+
+        items[j] = current;
+    }
 }
--- BasicSorting/main.cpp
+++ BasicSorting/main.cpp
@@ -22,7 +22,7 @@ int main(int argc, char** argv)
                                Item("Castor", 5)
                                };
 
-    auto sortingAlgorithm = utils::createSort(SortMethod::Insertion, items);
+    auto sortingAlgorithm = utils::createSort(SortMethod::OInsertion, items);
 
     utils::viewKeys(sortingAlgorithm->sort());
 
